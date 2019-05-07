5c946885e45b4e0b2dc26c8bb87ba1bd5ebbc74f
tm <327145559@qq.com>
2019-05-07 21:37:50 +0800

    话题列表页面

--- resources/views/topics/show.blade.php
+++ resources/views/topics/show.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
 
-<div class="container">
+<div class="container" style="margin-bottom:100px">
   <div class="col-md-10 offset-md-1">
     <div class="card ">
       <div class="card-header">
