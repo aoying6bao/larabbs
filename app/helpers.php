324e1fcec2d98b704ad87cf14b88a1d8a7de1f01
tm <327145559@qq.com>
2019-05-08 15:53:49 +0800

    分类话题列表

--- app/helpers.php
+++ app/helpers.php
@@ -3,3 +3,8 @@
     {
         return str_replace( '.' , '-', \Illuminate\Support\Facades\Route::currentRouteName() );
     }
+
+    function category_nav_active($category_id)
+    {
+        return active_class((if_route('categories.show') && if_route_param('category', $category_id)));
+    }
